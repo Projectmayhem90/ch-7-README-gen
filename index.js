feb74231082062ff1e186f8820b865e5cec53654
NatJoshYoyo <joshuaobrien28@yahoo.com>
2023-01-03 23:48:21 -0500

    Added Description for index-js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// Required packages
+// Required packages to run Gen
+
+
 const inquirer = require('inquirer');
+
 const fs = require('fs');
+
 const generateMarkdown = require('./utils/generateMarkdown.js')
 
-// Array of questions to provide information for the generated README
+// Data added to section for README
 const questions = [
     {
         type: 'input',
@@ -17,56 +21,49 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'What is the name of your application?',
+        message: 'What is the Sofware Title?',
         name: 'appName',
     },
     {
         type: 'list',
-        message: 'Which license are you using for your application?',
+        message: 'Select a License below',
         name: 'license',
-        choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'None'],
+        choices: ['MIT', 'I donnt want to use one'],
     },
     {
         type: 'edtior',
-        message: 'Please enter a description of your application: ',
+        message: 'Add what your application does via selected text application: ',
         name: 'desc',
     },
     {
         type: 'editor',
-        message: 'Please provide installation instructions: ',
+        message: 'Add how you application is installed or what version users should use: ',
         name: 'install',
     },
     {
         type: 'editor',
-        message: 'Please provide usage instructions: ',
-        name: 'usage',
-    },
-    {
-        type: 'editor',
-        message: 'Please provide testing instructions: ',
+        message: 'Testing or Debug Information: ',
         name: 'testing',
     },
     {
         type: 'editor',
-        message: 'Please list the contributions to this application?',
-        name: 'contributors',
+        message: 'List collaborators',
+        name: 'collaborators',
     },
     {
         type: 'editor',
-        message: 'Please list any questions the user may have and provide answers: ',
+        message: 'ADD FAQ if needed: ',
         name: 'faq',
     }
 ];
 
-// Function to write the README file
 function writeToFile(fileName, data) {
     const finalREADME = generateMarkdown(data);
 
     fs.writeFile(fileName, finalREADME, (err) =>
         err ? console.log(err) : console.log('Successfully generated your README!'))
 };
-
-// Function to initialize the application
+//Init Function
 function init() {
     const fileName = 'generatedREADME.md';
     inquirer.prompt(questions)
@@ -75,5 +72,7 @@ function init() {
 })
 };
 
-// Calling the function that will initialize the application
+
+
+//Add function
 init();
